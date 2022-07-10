e7f804a0f381d6cb4e0d20010e13c16159b64eeb
archpider <archpider@gmail.com>
2022-07-10 19:39:27 +0200

    fix error

--- main.tf
+++ main.tf
@@ -9,20 +9,21 @@ terraform {
 provider "libvirt" {
   uri = "qemu:///system"
 }
+
 resource "libvirt_pool" "default" {
   name = "default"
   type = "dir"
   path = "/var/lib/libvirt/images/"
 }
 
-resource "libvirt_volume" "almalinux8-qcow2" {
+resource "libvirt_volume" "centos8-qcow2" {
   name = "centos8.qcow2"
   pool = "default"
   source = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
   format = "qcow2"
 }
 
-resource "libvirt_domain" "test1" {
+resource "libvirt_domain" "homework" {
   name = "homework"
   memory = "2048"
   vcpu = 2
@@ -32,7 +33,7 @@ resource "libvirt_domain" "test1" {
   }
 
   disk {
-    volume_id = "${libvirt_volume.almalinux8-qcow2.id}"
+    volume_id = "${libvirt_volume.centos8-qcow2.id}"
   }
 
   console {
