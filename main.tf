5f6a0c60b2365ba0c8be1f6ff4104a6c2b6d719e
archpider <archpider@gmail.com>
2022-07-10 13:55:09 +0200

    homework vm config

--- main.tf
+++ main.tf
@@ -9,3 +9,42 @@ terraform {
 provider "libvirt" {
   uri = "qemu:///system"
 }
+resource "libvirt_pool" "default" {
+  name = "default"
+  type = "dir"
+  path = "/var/lib/libvirt/images/"
+}
+
+resource "libvirt_volume" "almalinux8-qcow2" {
+  name = "centos8.qcow2"
+  pool = "default"
+  source = "https://cloud.centos.org/centos/8/x86_64/images/CentOS-8-GenericCloud-8.4.2105-20210603.0.x86_64.qcow2"
+  format = "qcow2"
+}
+
+resource "libvirt_domain" "test1" {
+  name = "homework"
+  memory = "2048"
+  vcpu = 2
+
+  network_interface {
+    network_name = "default"
+  }
+
+  disk {
+    volume_id = "${libvirt_volume.almalinux8-qcow2.id}"
+  }
+
+  console {
+    type = "pty"
+    target_type = "serial"
+    target_port = "0"
+  }
+
+  graphics {
+    type = "spice"
+    listen_type = "address"
+    autoport = true
+  }
+
+}
